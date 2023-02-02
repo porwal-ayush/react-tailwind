b5f9955d2cf3ed0d530ed3c593dafd3e2da9b4f6
Ayush Porwal <aporwal@concertai.com>
2023-02-02 19:12:19 +0000

    sign-in sign-out user screens to  package

---
+++ packages/user-screens/index.ts
@@ -0,0 +1,5 @@
+// from src/components
+import SignIn from '../../src/components/SignIn/SignIn';
+import SignUp from '../../src/components/SignUp/SignUp';
+
+export { SignIn, SignUp }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'packages/user-screens/index.ts'),
+      name: 'UserScreens',
+      fileName: 'user-screens'
+    }
+  },
   plugins: [react()],
 })
