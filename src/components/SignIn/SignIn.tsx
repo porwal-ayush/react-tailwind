a6ef09d58da0fc21db69a26a62485cade2c62cd0
Ayush Porwal <aporwal@concertai.com>
2023-02-02 09:46:47 +0000

    Update SignIn.tsx and SignUp.tsx

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,24 +1,33 @@
 function SignIn() {
   return (
     <>
-      <form className="flex flex-col items-center">
-        <div>
+      <form className="border boder-solid flex flex-col items-center gap-2 w-6/12">
+        <div className="w-80">
           <div>
             <label className="text-sm">Email</label>
           </div>
-          <div>
+          <div className="border border-solid border-black">
             <input name="email" type="email" placeholder="ayush@gmail.com" />
           </div>
         </div>
-        <div>
+        <div className="w-80">
           <div>
-            <label className="text-sm">Password </label>
+            <label className="text-sm">Password</label>
           </div>
-          <div>
+          <div className="border border-solid border-black">
             <input type="password" placeholder="8+ characters needed" />
           </div>
         </div>
-        <button>Login</button>
+        <div></div>
+        <div className="w-80 border-2 border-solid border-purple-400 rounded">
+          <button type="button" className="height-48 w-full p-1 rounded">
+            Login
+          </button>
+        </div>
+        <div className="w-80 flex flex-row-reverse">
+          <p className="text-sm">Forgot Password?</p>
+        </div>
+        <div className="before:content-[''] after:content-['']">Or</div>
       </form>
     </>
   );
---
+++ src/components/SignUp/SignUp.tsx
@@ -0,0 +1,5 @@
+function SignUp() {
+  return <>Hello</>;
+}
+
+export default SignUp;
